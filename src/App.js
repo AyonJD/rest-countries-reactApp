3411a8ad706e23b40568c40b1395831c3a4c9f3c
Ayon <ayonjodder177@gmail.com>
2022-03-17 14:53:56 +0600

    Load countries using useEffect()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
 }
 
 export default App;
+
+const ShowCountry = props => {
+  const { name, population, flags } = props.country;
+  return (
+    <div>
+      <h3>Name: { name.common }</h3>
+      <p>Population: { population }</p>
+      <img src={ flags.png } alt="" />
+    </div>
+  )
+}
+
+const LoadCountries = () => {
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+  // console.log(countries);
+  return (
+    <div>
+      {
+        countries.map(country => <ShowCountry country={country} key={ country.cca3 }></ShowCountry>)  //Used a unique id for key here to avoid the warinng in console "add unique key for each child"
+      }
+    </div>
+  )
+}
